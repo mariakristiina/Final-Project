1f3b4280cae5a94d55818b9bf6cf4dee86734e27
Maria Palonen <mariakristiina@Marias-MacBook-Pro.local>
2019-12-04 12:33:10 +0100

    auth done

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,65 @@ const express = require("express");
 const passport = require('passport');
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
+router.post("/signup", (req, res) => {
+  const { username, password } = req.body
 
+  if(!username) {
+    return res.status(400).json({ message: "Please enter a Username"});
+  }
+if(password.length < 8) {
+  return res.status(400).json({ message: "Password is too short"})
+}
+
+User.findOne({ username: username })
+.then(found => {
+  if(found) {
+    return res.status(400).json({ message: "Username is already taken"});
+  }
+  return bcrypt
+  .genSalt()
+  .then(salt => {
+    return bcrypt.hash(password, salt);
+  })
+  .then(hash=> {
+    return User.create({ username: username, password: hash});
+  })
+  .then(newUser => {
+    req.login(newUser, err => {
+      if(err) res.status(500).json(err);
+      else res.json(newUser);
+    });
+  });
+})
+.catch(err => {
+  res.status(500).json(err);
+});
+});
+
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user) => {
+    if(err) {
+      return res.status(500).json({ message: "Error while authenticating" });
+    }
+    if(!user) {
+      return res.status(400).json({ message: "Invalid credentials"});
+    }
+    req.login(user, err => {
+      if(err) res.status(500).json(err);
+      res.json(user);
+    })
+  })(req, res, next);
+});
+
+router.delete("/logout", (req, res) => {
+  req.logout();
+  res.json({ message: "Successful logout" });
+});
+
+router.get("/loggedin", (req, res) => {
+  res.json(req.user);
+})
 
 module.exports = router;
