6304d25da30ffa84ce4f798a33298024b6b98c4f
NinoGlonti <54588901+NinoGlonti@users.noreply.github.com>
2019-12-04 15:05:23 +0100

    components created

---
+++ client/src/services/auth.js
@@ -0,0 +1,32 @@
+import axios from "axios"
+
+const signup = (username, password, age) => {
+  return axios.post("/auth/signup", {
+       username: username,
+       password: password,
+       age: age
+  }).then(respose => {
+    return response.data
+  }).catch(err => {
+    return err.response.data
+  })
+  
+}
+
+
+const login = (username, password) => {
+  return axios.post("/auth/login", {
+     username: username,
+     password: password
+  }).then (response => {
+    return response.data
+  }).catch(err => {
+    return err.response.data
+  })
+}
+
+const logout = () => {
+  axios.delete("/auth/logout")
+}
+
+export { signup, login, logout };
