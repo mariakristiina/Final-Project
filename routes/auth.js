23061734f4c4966bd4d3998671bc1d7951a697a9
Maria Palonen <mariakristiina@Marias-MacBook-Pro.local>
2019-12-05 09:57:51 +0100

    mergging

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const passport = require('passport');
 const router = express.Router();
 const User = require("../models/User");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 
 router.post("/signup", (req, res) => {
   const { username, password } = req.body
