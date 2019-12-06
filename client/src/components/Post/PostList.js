58bf7ff85a4b8c28b7efb7f2a716f1bf019bd1f8
toskaMaliske <toska.maliske@icloud.com>
2019-12-06 10:01:12 +0100

    posts link

--- client/src/components/Post/PostList.js
+++ client/src/components/Post/PostList.js
@@ -11,8 +11,11 @@ const PostList = props => {
             <h3>
               <Link to={`/post/${post._id}`}>{post.title}
               </Link>
+              
             </h3>
-            <p>{post.date}</p>
+            <p>User: {post.owner.username}</p>
+            <p>Date: {post.date}</p>
+            <p>Type: {post.postType}</p>
           </div>
         );
       })}
