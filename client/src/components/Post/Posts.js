7b26ffa67d6f0369ee5f96727af56b26bf9847d0
Maria Palonen <mariakristiina@Marias-MacBook-Pro.local>
2019-12-06 20:42:10 +0100

    merged

--- client/src/components/Post/Posts.js
+++ client/src/components/Post/Posts.js
@@ -4,10 +4,6 @@
 
 //filter for categories, date, owner, match
 import React, { useEffect } from "react";
-<<<<<<< HEAD
-//import PostForm from "./PostForm"
-=======
->>>>>>> 92e494c5f500dd5e165f98a5710a98aaf603ea97
 import { Link } from "react-router-dom";
 
 const Posts = props => {
