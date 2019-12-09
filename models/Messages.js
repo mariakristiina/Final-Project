fde63a447d01894bd4eb9a3241226692b63dc2c4
NinoGlonti <54588901+NinoGlonti@users.noreply.github.com>
2019-12-09 18:20:34 +0100

    "inbox done"

--- models/Messages.js
+++ models/Messages.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
-  title: String,
   content: String,
   subject: {
     type: Schema.Types.ObjectId,
